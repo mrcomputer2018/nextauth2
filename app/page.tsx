6d580620f7409f19fe9b3ce6a9a785b656ac59c6
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-02-06 16:56:49 -0300

    Add user session and server session to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import  Link from "next/link";
 import { buttonVariants } from "./_components/ui/button";
+import { User } from "lucide-react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./_lib/auth";
 
 
-export default function Home() {
+export default async function Home() {
+
+    const session = await getServerSession(authOptions);
+    
     return (
         <div>
             <h1 className='text-4xl'>
@@ -11,6 +17,11 @@ export default function Home() {
             <Link className={buttonVariants()} href="/admin">
                 Open My Admin
             </Link>
+
+            <h2>Client Session</h2>
+            <User />
+            <h2>Server Session</h2>
+            { JSON.stringify(session, null, 2) }
         </div>
     );
 }
