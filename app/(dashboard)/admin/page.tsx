ff039c93e518f8cf62d3a79c65f3e2ae40a4fe4d
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-02-06 14:34:33 -0300

    Add environment variable and update admin page***

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,6 +1,11 @@
-const Page = () => {
+import { authOptions } from "@/app/_lib/auth";
+import { getServerSession } from "next-auth";
+
+const Page = async () => {
+    const session = await getServerSession(authOptions);
+
     return ( 
-        <h1>Welcomwe</h1>
+        <h1>Welcome to admin</h1>
      );
 }
  
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import  Link from "next/link";
+import { buttonVariants } from "./_components/ui/button";
 
 
 export default function Home() {
     return (
-        <h1 className='text-4xl'>
-            Home
-        </h1>
+        <div>
+            <h1 className='text-4xl'>
+                Home
+            </h1>
+            <Link className={buttonVariants()} href="/admin">
+                Open My Admin
+            </Link>
+        </div>
     );
 }
